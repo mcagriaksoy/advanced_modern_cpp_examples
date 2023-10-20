42aa923e6810f081a433b1cb549e9d92b809f0d3
Mehmet Cagri Aksoy <mcagriaksoy@yandex.com>
2023-10-20 20:10:55 +0200

    Addiational examples added
    Signed-off-by: Mehmet Cagri Aksoy <mcagriaksoy@yandex.com>

---
+++ addiational_example_1.cpp
@@ -0,0 +1,63 @@
+// Mehmet Cagri Aksoy
+
+#include <iostream>
+
+// Class Shapes hold multiple circles and squares.
+class Circle
+{
+public:
+    int radius;
+};
+
+class Square
+{
+public:
+    int side;
+};
+
+class Shapes : public Circle, public Square
+{
+public:
+    int circles_radius()
+    {
+        return Circle::radius;
+    }
+
+    int squares_side()
+    {
+        return Square::side;
+    }
+
+    Shapes operator+(const Shapes &other)
+    {
+        Shapes result;
+        result.radius = this->radius + other.radius;
+        result.side = this->side + other.side;
+        return result;
+    }
+};
+
+int main()
+{
+
+    Shapes shapes1;
+    shapes1.radius = 10;
+    shapes1.side = 20;
+
+    Shapes shapes2;
+    shapes2.radius = 20;
+    shapes2.side = 30;
+
+    Shapes shapes3 = shapes1 + shapes2;
+
+    std::cout << "Shapes 1 radius: " << shapes1.circles_radius() << std::endl;
+    std::cout << "Shapes 1 side: " << shapes1.squares_side() << std::endl;
+
+    std::cout << "Shapes 2 radius: " << shapes2.circles_radius() << std::endl;
+    std::cout << "Shapes 2 side: " << shapes2.squares_side() << std::endl;
+
+    std::cout << "Shapes 3 radius: " << shapes3.circles_radius() << std::endl;
+    std::cout << "Shapes 3 side: " << shapes3.squares_side() << std::endl;
+
+    return 0;
+}
